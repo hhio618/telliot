39beba1c4a141179668324b17874363ceb427d36
ox26a <hhio618@gmail.com>
2021-04-04 22:27:20 +0430

    subscribing works

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
-	"github.com/tellor-io/telliot/pkg/contracts/tellor"
 )
 
 type ProfitChecker struct {
@@ -35,7 +37,7 @@ func (self *ProfitChecker) monitorReward() {
 	defer ticker.Stop()
 
 	var sub event.Subscription
-	events := make(chan *tellor.ITellorNonceSubmitted)
+	events := make(chan types.Log)
 
 	for {
 		sub, err = self.Sub(events)
@@ -74,18 +76,13 @@ func (self *ProfitChecker) monitorReward() {
 	}
 }
 
-func (self *ProfitChecker) Sub(output chan *tellor.ITellorNonceSubmitted) (event.Subscription, error) {
-	var tellorFilterer *tellor.ITellorFilterer
-	tellorFilterer, err := tellor.NewITellorFilterer(common.HexToAddress("0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"), self.client)
-	if err != nil {
-		return nil, errors.Wrap(err, "getting ITellorFilterer instance")
+func (self *ProfitChecker) Sub(output chan<- types.Log) (event.Subscription, error) {
+	q := ethereum.FilterQuery{
+		Addresses: []common.Address{common.HexToAddress("0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0")},
+		Topics:    [][]common.Hash{{common.HexToHash("0x9d2e5f03fc65aff196e0f3a8dd924b24099de487e8cffc888921d420ab196e39")}},
 	}
-	sub, err := tellorFilterer.WatchNonceSubmitted(
-		nil,
-		output,
-		nil,
-		nil,
-	)
+
+	sub, err := self.client.SubscribeFilterLogs(context.TODO(), q, output)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting  channel")
 	}
