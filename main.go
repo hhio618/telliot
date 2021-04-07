4ecc20769d44ee009bb4c887e48e3f51a231c20a
ox26a <hhio618@gmail.com>
2021-04-07 11:44:26 +0430

    watch for NewChallenge

--- main.go
+++ main.go
@@ -4,43 +4,38 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/big"
+	"math/rand"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
+	"github.com/tellor-io/telliot/pkg/contracts/tellor"
 )
 
-type ProfitChecker struct {
-	client *ethclient.Client
-}
-
 func main() {
-	client, err := ethclient.Dial("NODE URL")
+	client, err := ethclient.Dial(os.Getenv("NODE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	self := &ProfitChecker{
-		client: client,
+	contract, err := tellor.NewITellor(tellorAddress, client)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	self.monitorReward()
-}
-
-func (self *ProfitChecker) monitorReward() {
-	var err error
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
 	var sub event.Subscription
-	events := make(chan types.Log)
+	events := make(chan *tellor.TellorNonceSubmitted)
 
 	for {
-		sub, err = self.Sub(events)
+		sub, err = subscribe(client, events)
 		if err != nil {
 			log.Print("msg", "initial subscribing to  events failed")
 			<-ticker.C
@@ -48,7 +43,8 @@ func (self *ProfitChecker) monitorReward() {
 		}
 		break
 	}
-
+	var ctx context.Context
+	var lastCncl context.CancelFunc
 	for {
 		select {
 		case err := <-sub.Err():
@@ -58,10 +54,9 @@ func (self *ProfitChecker) monitorReward() {
 					" subscription error",
 					"err", err)
 			}
-
 			// Trying to resubscribe until it succeeds.
 			for {
-				sub, err = self.Sub(events)
+				sub, err = subscribe(client, events)
 				if err != nil {
 					log.Print("msg", "re-subscribing to  events failed")
 					<-ticker.C
@@ -71,20 +66,84 @@ func (self *ProfitChecker) monitorReward() {
 			}
 			log.Print("msg", "re-subscribed to  events")
 		case event := <-events:
-			fmt.Println("vLog tranfer", event)
+			if lastCncl != nil {
+				lastCncl()
+			}
+			if event.Slot.Uint64() == 3 {
+				ctx, lastCncl = context.WithCancel(context.Background())
+				go estimate(ctx, contract, client)
+			}
 		}
 	}
 }
 
-func (self *ProfitChecker) Sub(output chan<- types.Log) (event.Subscription, error) {
-	q := ethereum.FilterQuery{
-		Addresses: []common.Address{common.HexToAddress("0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0")},
-		Topics:    [][]common.Hash{{common.HexToHash("0x9d2e5f03fc65aff196e0f3a8dd924b24099de487e8cffc888921d420ab196e39")}},
+func subscribe(client *ethclient.Client, output chan *tellor.TellorNonceSubmitted) (event.Subscription, error) {
+	var tellorFilterer *tellor.TellorFilterer
+	tellorFilterer, err := tellor.NewTellorFilterer(common.HexToAddress("0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"), client)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting ITellorFilterer instance")
 	}
-
-	sub, err := self.client.SubscribeFilterLogs(context.TODO(), q, output)
+	sub, err := tellorFilterer.WatchNonceSubmitted(
+		nil,
+		output,
+		nil,
+		nil,
+	)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting  channel")
 	}
 	return sub, nil
+}
+
+var (
+	tellorAddress = common.HexToAddress("0x88df592f8eb5d7bd38bfef7deb0fbc02cf3778a0")
+)
+
+func estimate(ctx context.Context, contract *tellor.ITellor, client *ethclient.Client) {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		gasPrice, err := client.SuggestGasPrice(ctx)
+		if err != nil {
+			log.Print(err)
+			<-ticker.C
+			continue
+		}
+		abiP, err := abi.JSON(strings.NewReader(tellor.TellorABI))
+		if err != nil {
+			log.Fatal(err)
+		}
+		vars, err := contract.GetNewCurrentVariables(nil)
+		if err != nil {
+			log.Print(err)
+			<-ticker.C
+			continue
+		}
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		reqVals := [5]*big.Int{
+			new(big.Int).SetUint64(r.Uint64()),
+			new(big.Int).SetUint64(r.Uint64()),
+			new(big.Int).SetUint64(r.Uint64()),
+			new(big.Int).SetUint64(r.Uint64()),
+			new(big.Int).SetUint64(r.Uint64()),
+		}
+		packed, err := abiP.Pack("submitMiningSolution", "", vars.RequestIds, reqVals)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data := ethereum.CallMsg{
+			From:     common.HexToAddress("0xDD6D1C35518fc955BBBeb52C5c1f5Fb4E16D7EAF"),
+			To:       &tellorAddress,
+			GasPrice: gasPrice,
+			Data:     packed,
+		}
+		gasUsed, err := client.EstimateGas(ctx, data)
+		if err != nil {
+			log.Printf("while estimate gas: %v", err)
+			<-ticker.C
+			continue
+		}
+		fmt.Println("gasUsed", gasUsed)
+		break
+	}
+
 }
