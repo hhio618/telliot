afa07ce739bb111fb1adfb6de624975824e8aec2
hh <hhio618@gmail.com>
2021-06-25 12:30:32 +0430

    filter nonce

--- main.go
+++ main.go
@@ -3,91 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
+	"os"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
 	"github.com/tellor-io/telliot/pkg/contracts/tellor"
 )
 
-type ProfitChecker struct {
+type RefundChecker struct {
 	client *ethclient.Client
 }
 
 func main() {
-	client, err := ethclient.Dial("NODE URL")
+	client, err := ethclient.Dial(os.Getenv("NODE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	self := &ProfitChecker{
+	self := &RefundChecker{
 		client: client,
 	}
-
-	self.monitorReward()
+	self.monitorRefund()
 }
 
-func (self *ProfitChecker) monitorReward() {
+func (self *RefundChecker) monitorRefund() {
 	var err error
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
 
-	var sub event.Subscription
-	events := make(chan *tellor.ITellorNonceSubmitted)
-
-	for {
-		sub, err = self.Sub(events)
-		if err != nil {
-			log.Print("msg", "initial subscribing to  events failed")
-			<-ticker.C
-			continue
-		}
-		break
+	iter, err := self.TellorNonceIterator()
+	if err != nil {
+		log.Fatalf("getting TellorNonceIterator: %v", err)
 	}
-
-	for {
-		select {
-		case err := <-sub.Err():
-			if err != nil {
-				log.Print(
-					"msg",
-					" subscription error",
-					"err", err)
-			}
-
-			// Trying to resubscribe until it succeeds.
-			for {
-				sub, err = self.Sub(events)
-				if err != nil {
-					log.Print("msg", "re-subscribing to  events failed")
-					<-ticker.C
-					continue
-				}
-				break
-			}
-			log.Print("msg", "re-subscribed to  events")
-		case event := <-events:
-			fmt.Println("vLog tranfer", event)
-		}
+	for iter.Next() {
+		fmt.Printf("iter: %v", iter)
 	}
 }
 
-func (self *ProfitChecker) Sub(output chan *tellor.ITellorNonceSubmitted) (event.Subscription, error) {
+func (self *RefundChecker) TellorNonceIterator() (*tellor.ITellorNonceSubmittedIterator, error) {
 	var tellorFilterer *tellor.ITellorFilterer
 	tellorFilterer, err := tellor.NewITellorFilterer(common.HexToAddress("0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"), self.client)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting ITellorFilterer instance")
 	}
-	sub, err := tellorFilterer.WatchNonceSubmitted(
-		nil,
-		output,
+	iter, err := tellorFilterer.FilterNonceSubmitted(
+		&bind.FilterOpts{
+			Start: 12689004,
+			End:   nil,
+		},
 		nil,
 		nil,
 	)
 	if err != nil {
 		return nil, errors.Wrap(err, "getting  channel")
 	}
-	return sub, nil
+	return iter, nil
 }
